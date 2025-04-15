cf587631bf74cf8db9ee6ec17c775a0f0b51974a
free-bit-dot <oleksandr.ambroz-ip213@nung.edu.ua>
2025-04-15 16:42:36 +0300

    Add files via upload

---
+++ лр7/jest/math.test.js
@@ -0,0 +1,19 @@
+const { subtract, multiply, divide } = require('../../лр3/math');
+
+describe('Math functions', () => {
+  test('subtracts 5 - 2 to equal 3', () => {
+    expect(subtract(5, 2)).toBe(3);
+  });
+
+  test('multiplies 3 * 4 to equal 12', () => {
+    expect(multiply(3, 4)).toBe(12);
+  });
+
+  test('divides 10 / 2 to equal 5', () => {
+    expect(divide(10, 2)).toBe(5);
+  });
+
+  test('divides by 0 returns Infinity', () => {
+    expect(divide(5, 0)).toBe(Infinity);
+  });
+});
